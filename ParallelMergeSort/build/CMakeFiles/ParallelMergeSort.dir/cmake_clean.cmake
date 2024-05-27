d2a78aa31dd56a697b196af48d35bca83c0045f6
Lung Iustin <iustin.lung03@e-uvt.ro>
2024-05-28 00:15:56 +0300

    Initial commit: Add parallel merge sort implementation

---
+++ ParallelMergeSort/build/CMakeFiles/ParallelMergeSort.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ParallelMergeSort.dir/parallel_merge_sort.cpp.o"
+  "CMakeFiles/ParallelMergeSort.dir/parallel_merge_sort.cpp.o.d"
+  "ParallelMergeSort"
+  "ParallelMergeSort.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ParallelMergeSort.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ParallelMergeSort/build/CMakeFiles/ParallelMergeSort.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for ParallelMergeSort.
+# This may be replaced when dependencies are built.
